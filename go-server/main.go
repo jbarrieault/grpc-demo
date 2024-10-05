1fb53f7dfb29bb3e910da7004c80e68ce29baf66
Jacob Barrieault <jacob.barrieault@gmail.com>
2024-10-05 07:35:21 -0400

    Implement an Echo Service

--- go-server/main.go
+++ go-server/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
 
+	pb "github.com/jbarrieault/grpc-demo/services/echo"
+	"google.golang.org/grpc"
+)
+
+type echoServer struct {
+	pb.UnimplementedEchoServer
+}
+
+func (*echoServer) Echo(_ context.Context, message *pb.EchoMessage) (*pb.EchoMessage, error) {
+	log.Printf("Received Echo: %v", message.GetValue())
+	return &pb.EchoMessage{Value: message.GetValue()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("net.Listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &echoServer{})
+
+	log.Println("Echo Service listening on port 3000")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
---
+++ services/echo/go.mod
@@ -0,0 +1,3 @@
+module github.com/jbarrieault/grpc-demo/services/echo
+
+go 1.22.6
